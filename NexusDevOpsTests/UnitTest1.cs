ad1157b5206a8e0fcedd359cfd72e5f8a336aede
Orin Parris <orin.parris@gmail.com>
2022-03-10 22:30:55 -0400

    added devopstests

---
+++ NexusDevOpsTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace NexusDevOpsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var a = 5;
+            var b = 6;
+            var c = a+b;
+            Assert.IsTrue(c==11);
+        }
+    }
+}
